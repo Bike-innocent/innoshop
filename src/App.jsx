567ccb05feceb489194ba0f499556b42571ce6e0
Bike-innocent <onyemaobichibuikeinnocent.com@gmail.com>
2024-12-27 12:39:42 +0100

    Fix config error

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ const router = createBrowserRouter([
       { path: '/product/:slug', element: <ProductDetails /> },
       { path: '/offcanvas', element: <OffCanvasComponent /> },
       { path: '/products', element: <Products /> },
-      { path: '/aa', element: <SkeletonLoader /> },
+      { path: '/aaa', element: <SkeletonLoader /> },
 
       // Dashboard Route
       {
